bae9f7d0684b45e5a64ff6cc8e7c9feed052b022
cristian15240613 <15240613@itleon.edu.mx>
2018-09-12 22:07:10 -0500

    Proyecto semaforo V2 Se le agrego la funcionalidad con el potenciometro

--- ProyectoSemaforo.ino
+++ ProyectoSemaforo.ino
@@ -5,6 +5,9 @@ int c_v = 11;
 int p_v = 10;
 int p_r = 9;
 
+int potenciometro = A0;
+int valPot = 0;
+
 void setup() {
   pinMode(c_r, OUTPUT);
   pinMode(c_a, OUTPUT);
@@ -12,18 +15,31 @@ void setup() {
 
   pinMode(p_v, OUTPUT);
   pinMode(p_r, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  valPot = analogRead(potenciometro);
+  int valVerde = valPot * 40;
+  int segundos = valVerde / 1000;
+  /*  Valor de potenciometro
+  Serial.println(valPot);
+  delay(1000);
+  */
+  Serial.print("El semaforo en verde durará: ");
+  Serial.print(segundos);
+  Serial.print(" segundos más dos parpadeos");
+  Serial.println(" ");
+
+  // Encender led rojo de peaton y apagar led verde de peaton
   digitalWrite(p_v, LOW);
   digitalWrite(p_r, HIGH);
   
   digitalWrite(c_v, HIGH);
-  delay(5000);
-  digitalWrite(c_v, LOW);
-  delay(500);
+  delay(valVerde);
 
-  for(int i=0; i < 2; i++){
+  for(int i=0; i < 3; i++){
     digitalWrite(c_v, HIGH);
     delay(500);
     digitalWrite(c_v, LOW);
@@ -36,20 +52,21 @@ void loop() {
   digitalWrite(c_a, LOW);
   delay(500);
 
+  // Encender led verde de peaton y apagar led rojo de peaton
   digitalWrite(p_r, LOW);
   digitalWrite(p_v, HIGH);
   
   digitalWrite(c_r, HIGH);
   delay(4500);
-  digitalWrite(c_r, LOW);
-  delay(500);
 
-  for(int i=0; i < 2; i++){
+  for(int i=0; i < 3; i++){
     digitalWrite(c_r, HIGH);
+    digitalWrite(p_v, HIGH);
+        
     delay(500);
-    digitalWrite(c_r, LOW);
-    delay(500);  
-    
-  }
 
+    digitalWrite(p_v, LOW);
+    digitalWrite(c_r, LOW);
+    delay(500);
+  }
 }
